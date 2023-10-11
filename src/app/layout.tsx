1a44da96ae3653649e15957c5a4ed75d68820345
Guilherme <gui25ber@gmail.com>
2023-10-11 01:11:32 -0400

    feat: included a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Tridimenzion',
   description:
-    'Tridimenzion is a gallery of 3D ART, showcasing various artworks created using Three.js, React Three Fiber, and Drei.'
+    'Tridimenzion is a gallery of 3D ART, showcasing various artworks created using Three.js, React Three Fiber, and Drei.',
+  icons: {
+    icon: 'img/favicon/favicon.ico'
+  }
 }
 
 export default function RootLayout({
